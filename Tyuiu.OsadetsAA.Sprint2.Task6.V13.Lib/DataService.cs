cacb1dbe67921a00c04cc9990fe7a73467d0b3be
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-10-16 17:45:29 +0500

    Task6

---
+++ Tyuiu.OsadetsAA.Sprint2.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,88 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.OsadetsAA.Sprint2.Task6.V13.Lib
+{
+    public class DataService : ISprint2Task6V13
+    {
+        public string FindDateOfNextDay(int g, int m, int n)
+        {
+            switch (m)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                    if (n < 31)
+                    {
+                        n++;
+                    }
+                    else
+                    {
+                        n = 1;
+                    }
+                    break;
+                case 2:
+                    if (n < 29)
+                    {
+                        n++;
+                    }
+                    else
+                    {
+                        n = 1;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (n < 30)
+                    {
+                        n++;
+                    }
+                    else
+                    {
+                        n = 1;
+                    }
+                    break;
+                case 12:
+                    if (n < 31)
+                    {
+                        n++;
+                    }
+                    else
+                    {
+                        n = 1;
+                        m = 1;
+                        g++;
+                    }
+                    break;
+            }
+            string y = Convert.ToString(g);
+            string z = Convert.ToString(m);
+            string x = Convert.ToString(n);
+            if (n < 10 && m < 10)
+            {
+                return "0" + x + "." + "0" + z + "." + y;
+            }
+            else
+            {
+                if (n < 10)
+                {
+                    return "0" + x + "." + z + "." + y;
+                }
+                else
+                {
+                    if (m < 10)
+                    {
+                        return x + "." + "0" + z + "." + y;
+                    }
+                    else
+                    {
+                        return x + "." + z + "." + y;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tyuiu.OsadetsAA.Sprint2.Task6.V13/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.OsadetsAA.Sprint2.Task6.V13.Lib;
+namespace Tyuiu.OsadetsAA.Sprint2.Task6.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = ("Спринт #2 | Выполнил: Осадец А. А. | ПКТб-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Осадец Александр Александрович | ПКТб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int g, m, n;
+            g = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            var res = ds.FindDateOfNextDay(g, m, n);
+            Console.WriteLine(res);
+        }
+    }
+}
